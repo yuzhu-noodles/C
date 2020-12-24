ebe035a05fbaffe342c5503eac656cbcf0ca245a
liqi <qli@snnu.deu.cn>
2020-12-24 14:12:44 +0800

    提交一个简单的扫雷小程序

---
+++ 扫雷/扫雷/test.c
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"game.h"
+void menu()
+{
+	printf("*************************\n");
+	printf("**********1.play*********\n");
+	printf("**********0.exit*********\n");
+	printf("*************************\n");
+}
+void game()
+{
+	//雷的信息存储
+	//1、布置好雷的信息
+	char mine[ROWS][COLS] = {0};
+	//2、排查出雷的信息
+	char show[ROWS][COLS] = {0};
+	//初始化
+	InitBoard(mine, ROWS, COLS,'0');
+	InitBoard(show, ROWS, COLS,'*');
+	//打印棋盘
+	DisplayBoard(mine, ROW, COL);
+	DisplayBoard(show, ROW, COL);
+	//布置雷
+	SetMine(mine, ROW, COL);
+	DisplayBoard(mine, ROW, COL);
+	//扫雷
+	FindMine(mine, show, ROW, COL);
+
+}
+void test()
+{
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择：>\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 0:
+			printf("退出游戏\n");
+			break;
+		case 1:
+			game();
+			break;
+		default:
+			printf("选择错误，请重新选择！\n");
+			break;
+		}
+	} while (input);
+}
+int main()
+{
+	test();
+	return 0;
+}
