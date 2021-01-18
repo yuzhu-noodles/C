b187696abaaa9239dcdc97a54b8e9060102244f3
yuzhu-noodles <qli@snnu.edu.cn>
2021-01-18 15:40:24 +0800

    在杨氏矩阵中查找一个数

---
+++ FindNum.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//时间复杂度<O(N)
+int FindNum(int arr[3][3],int k,int* px,int* py)
+{
+	int x = 0;
+	int y = *py - 1;
+	while (x <= *px - 1 && y >= 0)
+	{
+		if (arr[x][y] > k)
+		{
+			y--;
+		}
+		else if (arr[x][y] < k)
+		{
+			x++;
+		}
+		else
+		{
+			*px = x;
+			*py = y;
+			return 1;
+		}
+	}
+	//找不到
+	return 0;
+}
+int main()
+{
+	int arr[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	int k = 7;
+	int x = 3;
+	int y = 3;
+	int ret = FindNum(arr, k, &x, &y);
+	if (ret == 1)
+	{
+		printf("找到了\n");
+		printf("下标是：%d %d\n",x,y);
+	}
+	else
+	{
+		printf("未找到\n");
+	}
+	return 0;
+}
